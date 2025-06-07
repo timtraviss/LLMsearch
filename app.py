95dce4051b6f509638c4eb7d449c912a05b2c2c7
timtraviss <57478589+timtraviss@users.noreply.github.com>
2025-06-08 10:47:40 +1200

    Update app.py

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ import os
 # Load environment variables
 load_dotenv()
 
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     import uvicorn
+#     port = int(os.getenv("PORT", 8000))
+#     uvicorn.run("main:app", host="0.0.0.0", port=port)
 
 # Initialize APIs with error handling
 try:
